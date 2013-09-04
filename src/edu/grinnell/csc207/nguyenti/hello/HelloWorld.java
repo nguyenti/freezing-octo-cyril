8ad283c5f11cc769328f23a222b6fd785022e9ea
nguyenti <nguyenti@users.noreply.github.com>
2013-09-04 10:46:39 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/nguyenti/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/nguyenti/hello/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.grinnell.csc207.nguyenti.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Rawr!");
+        System.out.println("Banana!");
     } // main(String[])
 }
