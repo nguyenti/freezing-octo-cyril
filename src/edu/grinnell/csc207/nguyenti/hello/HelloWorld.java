13ab0775027a92a4cab523e35ec13b66b7f1185a
nguyenti <nguyenti@users.noreply.github.com>
2013-09-04 10:38:05 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/nguyenti/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/nguyenti/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.grinnell.csc207.nguyenti.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, from GitHub!");
+        System.out.println("Rawr!");
     } // main(String[])
 }
